c39e0a8efa10c95fb98d2d0b4281377840bd0f23
Dorian Krause <dorian@91df63fb.com>
2014-08-10 16:41:51 +0200

    ibtop: Add missing common.h file
    common.h provides the well-known likely()/unlikely() macros.

---
+++ ibtop/common.h
@@ -0,0 +1,9 @@
+
+#ifndef IBTOP_FABRIC_COMMON_H_INCLUDED
+#define IBTOP_FABRIC_COMMON_H_INCLUDED 1
+
+#define unlikely(x)	__builtin_expect(!!(x), 0)
+#define   likely(x)	__builtin_expect(!!(x), 1)
+
+#endif
+
